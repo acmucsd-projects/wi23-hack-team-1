cd128ed88744121fe241caed36f122bbc6ee9830
Kinan Alatasi <kinan@Kinans-MacBook-Air.local>
2023-02-25 12:58:17 -0800

    signUp page done

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,15 @@
-<<<<<<< HEAD
-//import logo from './logo.svg';
-import './App.css';
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import SignupPage from "./signupPage/signupPage"
-=======
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./Login/Login";
->>>>>>> 43372ad7fe93e57cf1f7f0151d4b9cbfd0425c47
+import SignupPage from './signupPage/signupPage';
 
 function App() {
   return (
     <Router>
     <div className="App">
       <Routes>
-<<<<<<< HEAD
         <Route exact path="/" element={<SignupPage />} />
-=======
         <Route exact path="/" element={<Login />} />
->>>>>>> 43372ad7fe93e57cf1f7f0151d4b9cbfd0425c47
       </Routes>
     </div>
     </Router>
--- client/src/signupPage/signupPage.jsx
+++ client/src/signupPage/signupPage.jsx
@@ -1,10 +1,11 @@
 import React, {useState} from "react";
 import FoodBankOutlinedIcon from '@mui/icons-material/FoodBankOutlined';
 import IconButton from '@mui/material/IconButton'
-//import "/.style.css";
+import "./signupPage.css";
 import { FormControl, InputAdornment, InputLabel, OutlinedInput, TextField, Button } from "@mui/material";
 import Visibility from '@mui/icons-material/Visibility'
 import VisibilityOff from "@mui/icons-material/Visibility";
+import restaurant from './restaurant.jpeg';
 
 //NEED TO ADD LOGO At toP, HOW??
 const SignupPage = () => {
@@ -16,51 +17,52 @@ const SignupPage = () => {
     const handleClickShowPassword = () => setShowPassword( (show) => !show);
     const handleMouseDownPassword = (event) => {
         event.preventDefault();
-    }
+    };
 
     return(
         <form>
-            <div className="signup-page-container" >  
-                <div className="signup-form">
-                    <div className = "foodIcon">
-                        <FoodBankOutlinedIcon sx={{ fontSize: "10rem" }}></FoodBankOutlinedIcon>
-                        <h1 id="app_name">IGFood</h1>
-                    </div>
-                    <h1 id="createAccount">Create your account</h1>
-                    <div className="name-row">
-                        <TextField id="outlined-basic" sx={{ width: "60%", backgroundColor: "#F5F5F5" }} 
-                        label="Full name" variant="outlined"
-                        value={name} onChange={(e) => setName(e.target.value)}/>   
-                    </div>
-                    <div className="address-row">
-                        <TextField id="outlined-basic" sx={{ width: "60%", backgroundColor: "#F5F5F5" }} 
-                        label="Email Address" variant="outlined"
-                        value={email} onChange={(e) => setEmail(e.target.value)}/>
-                    </div>
-                    <div className="password-row">
-                        <FormControl sx={{m: 1, width: "60%", backgroundColor: "#F5F5F5"}} variant="outlined">
-                            <InputLabel htmlFor="outlined-adornment-password">Enter Password</InputLabel>
-                            <OutlinedInput 
-                                id="outlined_adornment_password"
-                                type={showPassword ? 'text' : 'password'}
-                                endAdornment={
-                                    <InputAdornment position="end">
-                                        <IconButton aria-label="toggle password visibility"
-                                        onClick={handleClickShowPassword}
-                                        onMouseDown={handleMouseDownPassword}
-                                        edge="end"
-                                        >
-                                            {showPassword ? <VisibilityOff /> : <Visibility/>}
-                                        </IconButton>
-                                    </InputAdornment>
-                                }
-                                label="password"
-                                value={password} onChange={(e) => setPassword(e.target.value)}
-                                />
-                        </FormControl>
-                    </div>
-                    <Button id="create_account_bttn" variant="contained">Create your account</Button>
+            <div className="food_image">
+            <img src={restaurant} alt="Logo" />
+            </div>
+            <div className="signup-form">
+                <div className = "foodIcon">
+                    <FoodBankOutlinedIcon sx={{ fontSize: "10rem" }}></FoodBankOutlinedIcon>
+                    <h1 id="app_name">IGFood</h1>
                 </div>
+                <h1 id="createAccount">Create your account</h1>
+                <div className="create-row">
+                    <TextField id="outlined-basic" sx={{ width: "60%", backgroundColor: "#F5F5F5" }} 
+                    label="Full name" variant="outlined"
+                    value={name} onChange={(e) => setName(e.target.value)}/>   
+                </div>
+                <div className="create-row">
+                    <TextField id="outlined-basic" sx={{ width: "60%", backgroundColor: "#F5F5F5" }} 
+                    label="Email Address" variant="outlined"
+                    value={email} onChange={(e) => setEmail(e.target.value)}/>
+                </div>
+                <div className="create-row">
+                    <FormControl sx={{m: 1, width: "60%", backgroundColor: "#F5F5F5"}} variant="outlined">
+                        <InputLabel htmlFor="outlined-adornment-password">Enter Password</InputLabel>
+                        <OutlinedInput 
+                            id="outlined_adornment_password"
+                            type={showPassword ? 'text' : 'password'}
+                            endAdornment={
+                                <InputAdornment position="end">
+                                    <IconButton aria-label="toggle password visibility"
+                                    onClick={handleClickShowPassword}
+                                    onMouseDown={handleMouseDownPassword}
+                                    edge="end"
+                                    >
+                                        {showPassword ? <VisibilityOff /> : <Visibility/>}
+                                    </IconButton>
+                                </InputAdornment>
+                            }
+                            label="password"
+                            value={password} onChange={(e) => setPassword(e.target.value)}
+                            />
+                    </FormControl>
+                </div>
+                <Button id="create_account_bttn" variant="contained">Create your account</Button>
             </div>
         </form>
         
