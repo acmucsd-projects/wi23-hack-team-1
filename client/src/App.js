3c17814677bef2efbc0a51afeb11b1b2a3eeedf2
Kinan Alatasi <kinan@Kinans-MacBook-Air.local>
2023-02-25 13:50:11 -0800

    all pages except feed done

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./Login/Login";
-<<<<<<< HEAD
 import SignupPage from './signupPage/signupPage';
-=======
 import Home from "./Home/Home";
 import Post from "./Post/Post";
 import ProfilePage from "./ProfilePage";
->>>>>>> bae57c5a100cea3c791d47e7cf755c30dd17bc54
 
 function App() {
   return (
