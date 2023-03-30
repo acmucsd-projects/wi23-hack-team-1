5893006f6f100caf7719da37d737ef26fccdfc8e
Kinan Alatasi <kinan@kinans-air.lan>
2023-03-30 13:43:39 -0700

    finished implementing autocomplete/marker placement

--- client/src/App.js
+++ client/src/App.js
@@ -5,17 +5,21 @@ import SignupPage from './signupPage/signupPage';
 import Home from "./Home/Home";
 import Post from "./Post/Post";
 import ProfilePage from "./ProfilePage";
+import Maps from "./Maps/Maps"
+require('dotenv').config()
+
 
 function App() {
   return (
     <Router>
     <div className="App">
       <Routes>
-        <Route exact path="/" element={<SignupPage />} />
+        <Route exact path = "/" element = {<Maps />} />
+        {/*<Route exact path="/" element={<SignupPage />} />
         <Route exact path="/" element={<Login />} />
         <Route exact path="/home" element={<Home />} />
         <Route exact path="/post" element={<Post />} />
-        <Route path="/profile" element={<ProfilePage />} />
+  <Route path="/profile" element={<ProfilePage />} /> */}
       </Routes>
     </div>
     </Router>
